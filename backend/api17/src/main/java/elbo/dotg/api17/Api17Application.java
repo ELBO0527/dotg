4d86d438a5e274cda28f3c670d940b6b798ffeed
ELBO0527 <63278770+ELBO0527@users.noreply.github.com>
2023-03-26 19:32:28 +0900

    project settings

---
+++ backend/api17/src/main/java/elbo/dotg/api17/Api17Application.java
@@ -0,0 +1,13 @@
+package elbo.dotg.api17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Api17Application.class, args);
+	}
+
+}
---
+++ backend/api17/src/test/java/elbo/dotg/api17/Api17ApplicationTests.java
@@ -0,0 +1,13 @@
+package elbo.dotg.api17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
