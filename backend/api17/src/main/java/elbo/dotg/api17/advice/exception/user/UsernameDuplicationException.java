362b85f491f3ab9f03726240b0cc8c047396c439
ELBO0527 <sbwar@naver.com>
2023-08-10 15:51:06 +0900

    feat/카테고리 생성, 조회 API

--- backend/api17/src/main/java/elbo/dotg/api17/advice/exception/user/UsernameDuplicationException.java
+++ backend/api17/src/main/java/elbo/dotg/api17/advice/exception/user/UsernameDuplicationException.java
@@ -1,7 +1,12 @@
 package elbo.dotg.api17.advice.exception.user;
 
 public class UsernameAlreadyExistsException extends RuntimeException {
-    //private static final String message = "해당 아이디는 이미 등록되어있습니다.";
+    private static final String message = "해당 아이디는 이미 등록되어있습니다.";
+
+    public UsernameAlreadyExistsException() {
+        super(message);
+    }
+
     public UsernameAlreadyExistsException(String message) {
         super(message);
     }
